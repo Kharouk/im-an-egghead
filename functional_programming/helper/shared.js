4ed31ef20984b7b1f6ed98100c128b495f1a04e8
Alex Kharouk <alex.kharouk@mvfglobal.com>
2019-10-04 16:05:52 +0100

    finish debug and the course

--- functional_programming/helper/shared.js
+++ functional_programming/helper/shared.js
@@ -1,6 +1,10 @@
 exports.compose = (...fns) => initialText =>
   fns.reduceRight((acc, fn) => fn(acc), initialText);
-
+exports.split = pattern => str => str.split(pattern);
+exports.join = separator => xs => xs.join(separator);
 exports.scream = str => str.toUpperCase();
 exports.exclaim = str => `${str}!`;
 exports.repeat = str => `${str} ${str}`;
+exports.lowerCase = str => str.toLowerCase();
+exports.upperCase = str => str.toUpperCase();
+exports.map = fn => array => array.map(fn);
