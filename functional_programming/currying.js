1ec4c06f159f1c5b0110f9aedfb50f7d90b90f99
Alex Kharouk <alex.kharouk@mvfglobal.com>
2019-10-04 11:08:16 +0100

    currying

---
+++ functional_programming/currying.js
@@ -0,0 +1,34 @@
+// Currying
+// The act of refactoring a function so that it receives its arguments one at a time; the act of taking a function that usually has more than one argument and becomes a Higher Order Function.
+
+const normalAdd = (x, y) => x + y;
+console.log(normalAdd(5, 3)); // 8
+
+// becomes:
+
+function add(x) {
+  return function(y) {
+    return x + y;
+  };
+}
+console.log(add(5, 6)); // returns [Function]
+// this happens because the second variable is ignored and just returns the function on line 10
+
+const addThree = add(3);
+
+console.log(addThree.toString());
+// function(y) {
+//   return x + y;
+// }
+
+console.log(addThree(7)); // 10
+
+// Every function in functional programming is curried like this
+
+// Refactored we can do this:
+const multiply = x => y => x * y;
+const multiplyBy10 = multiply(10);
+console.log(multiplyBy10(30)); // 300
+console.log(multiplyBy10(-23)); // -230
+
+//
